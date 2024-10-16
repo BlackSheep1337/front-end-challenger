3f35da85b34e3dad4f1717f6fbe9c708e2edda42
Ale Pereira <naitwa-alexandre@gmail.com>
2024-10-16 05:33:49 -0300

    refine sign in tab

--- app/pages/Login.tsx
+++ app/pages/Login.tsx
@@ -6,7 +6,7 @@ const Login = () => {
   return (
     <>
       <div className='flex mt-60 text-black'>
-        <div className='bg-white flex-1 h-96 p-8'>
+        <div className='bg-white flex-1 h-92 p-12'>
           <div className='flex justify-between hover:cursor-pointer'>
             <h3>Sign In</h3>
             <div className='flex gap-5 text-gray-400'>
@@ -26,12 +26,22 @@ const Login = () => {
                 <label className='text-left text-md font-bold uppercase' htmlFor="">Password</label>
                 <input autoComplete='off' placeholder='Password'  className='mt-2 pl-6 rounded-3xl bg-gray-100 w-full h-12' type="password" />
               </div>
-
             </div>
-            <button className='bg-[#FF4579] mt-6 w-full rounded-3xl text-white text-lg h-12'>Sign in</button>
+            <button className='bg-[#FF4579] mt-8 w-full rounded-3xl text-white text-lg h-12'>Sign in</button>
+            <div className='flex justify-between items-end mt-2'>
+              <div>
+                <input type="checkbox" id="remember-me" />
+                <label className='hover:cursor-pointer text-[#FF4579] text-xl ml-4 ' htmlFor="remember-me">Remember Me</label>
+              </div>
+              <div>
+                <p className='hover:cursor-pointer text-gray-400 text-xl'>
+                  Forget Passord
+                </p>
+              </div>
+            </div>
           </form>
         </div>
-        <div className='bg-[#FF4579] flex-1 h-96 p-8'>Welcome to Login</div>
+        <div className='bg-[#FF4579] flex-1 h-[30rem] p-8'>Welcome to Login</div>
       </div>
     </>
   )
