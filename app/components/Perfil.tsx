4eb698f12fad39f2a5e4bd8268b63bc345deac1f
Ale Pereira <naitwa-alexandre@gmail.com>
2024-10-17 11:32:11 -0300

    setup list of users

---
+++ app/components/Perfil.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Perfil = () => {
+  return (
+    <div className='bg-white max-w-3xl mx-auto my-8 p-12 text-black rounded-lg shadow-md'>
+      
+    </div>
+  )
+}
+
+export default Perfil
---
+++ app/components/UserList.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+interface IUserList {
+  name: string;
+  email: string;
+  street: string;
+  state: string;
+  city: string;
+  number: number;
+}
+
+const UserList = ({ data }: { data: IUserList[] }) => {
+  return (
+    <div className='bg-white max-w-3xl mx-auto my-8 p-12 text-black rounded-lg shadow-md'>
+      <div className='flex justify-between'>
+        <h1 className='text-3xl'>Lista de usuários cadastrados</h1>
+      </div>
+      <table className='table table-bordered mt-8'>
+        <thead>
+          <tr>
+            <th>
+              Nome
+            </th>
+            <th>
+              Email
+            </th>
+            <th>
+              Rua
+            </th>
+            <th>
+              Numero
+            </th>
+            <th>
+              Cidade
+            </th>
+            <th>
+              Estado
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+            {data.map(({name,email,street,state,city, number}) => {
+              return (
+                <tr className='hover:bg-slate-200 hover:cursor-pointer transition'>
+                  <td>{name}</td>
+                  <td>{email}</td>
+                  <td>{street}</td>
+                  <td>{number}</td>
+                  <td>{city}</td>
+                  <td>{state}</td>
+                </tr>
+              )
+            })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default UserList
